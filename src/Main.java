a41316089737982d371081b42cf29d2f562608e1
Igor Brum <igorsbrum@gmail.com>
2017-06-07 14:10:10 -0300

    Adicionado 20170607 003 - Criado metodos separando logica do desenho

--- src/Main.java
+++ src/Main.java
@@ -17,55 +17,29 @@ public class Main extends GraphicApplication implements MouseObserver {
 	public GameObject heroObject, containerObject;
 	public Maze maze;
 	public Room room;
+	public Item item[] = new Item[2];
 	
 	@Override
 	protected void setup() {
-		int randomRoomNumber;
-		int randomColor;
 		maze = new Maze();
 		room = new Room();
-
-		//ARQUIVO QUE CONTEM A CONFIGURACAO DE SALAS DO LABIRINTO
-		pathToFileDungeonMap = "files/Labirinto.txt";
-		pathToFileContainer = "img/square.png";
 		
-		//IMAGENS USADAS NOS SPRITES
-		pathToFileHero = "img/hero-noitem.png";
-		pathToFileDoor = "img/dungeon-door.png";
-		pathToFileKey = "img/key.png";
-		
+		//ARQUIVOS INICIAIS DE CONFIGURACAO
+		initialFiles();
 		
 		//TITULO DA TELA DO JOGO E RESOLUCAO
 		setTitle("MazeGame");
 		setResolution(Resolution.HIGHRES);
 		
-		//CRIACAO DO SPRITE DO HEROI
-		containerObject = GameObject.createObject(pathToFileContainer, 0, 525, Color.BLUE);
-		heroObject = GameObject.createObject(pathToFileHero, 375, 250, Color.BLUE);
-		
-		//CRIACAO DE SPRITES DAS PORTAS E ESCADAS (A.K.A SAIDAS) NORTE-SUL-OESTE-LESTE-ACIMA-ABAIXO (NESTA ORDEM)
-		doorObject[0] = GameObject.createObject(pathToFileDoor, 368, 10, Color.BLUE);
-		doorObject[1] = GameObject.createObject(pathToFileDoor, 368, 522, Color.BLUE);
-		doorObject[2] = GameObject.createObject(pathToFileDoor, 647, 254, Color.BLUE);
-		doorObject[3] = GameObject.createObject(pathToFileDoor, 93, 254, Color.BLUE);
-		doorObject[4] = GameObject.createObject(pathToFileDoor, 470, 150, Color.BLUE);
-		doorObject[5] = GameObject.createObject(pathToFileDoor, 230, 415, Color.BLUE);
-		
-		//CRIACAO DE SPRITES DAS CHAVES
-		keyObject[0] = GameObject.createObject(pathToFileKey, 230, 145, Color.BLUE);
-		keyObject[1] = GameObject.createObject(pathToFileKey, 600, 200, Color.BLUE);
-		keyObject[2] = GameObject.createObject(pathToFileKey, 350, 350, Color.BLUE);
-		keyObject[3] = GameObject.createObject(pathToFileKey, 600, 400, Color.BLUE);
-		
 		//INICIA O LABIRINTO
 		maze.createMaze(pathToFileDungeonMap);
 		
+		//CRIA OS OBJETOS DO LABIRINTO
+		createObjects();
+		
 		//POSICAO ALEATORIA INICIAL DAS CHAVES 
-		for (int i = 0; i < 4; i++) {
-			//randomRoomNumber = (int)(Math.random()*(31-1));
-			randomRoomNumber = 10;
-			randomColor = (int)(Math.random()*(3-0));
-			key[i] = maze.createKeys(randomRoomNumber, randomColor, keyObject[i], true);
+		for (int i = 0; i < key.length; i++) {
+			key[i] = randomKeys(i);
 		}
 		
 		//SALA INICIAL DO LABIRINTO EM MODO ALEATORIO
@@ -89,31 +63,24 @@ public class Main extends GraphicApplication implements MouseObserver {
 		containerObject.draw(canvas);
 		canvas.putText(10, 0, 30, "Sala Numero: "+roomNumber);
 		
-		if (room.getNorthNumber() >= 0) {
-			door[0] = maze.createDoor(room.getNorthNumber(), doorObject[0]);
+		if (hasNorthDoor()) {
 			door[0].getObj().draw(canvas);
 		}
-		if (room.getSouthNumber() >= 0) {
-			door[1] = maze.createDoor(room.getSouthNumber(), doorObject[1]);
+		if (hasSouthDoor()) {
 			door[1].getObj().draw(canvas);
 		}
-		if (room.getEastNumber() >= 0) {
-			door[2] = maze.createDoor(room.getEastNumber(), doorObject[2]);
+		if (hasEastDoor()) {
 			door[2].getObj().draw(canvas);
 		}
-		if (room.getWestNumber() >= 0) {
-			door[3] = maze.createDoor(room.getWestNumber(), doorObject[3]);
+		if (hasWestDoor()) {
 			door[3].getObj().draw(canvas);
 		}
-		if (room.getUpNumber() >= 0) {
-			door[4] = maze.createDoor(room.getUpNumber(), doorObject[4]);
+		if (hasUpDoor()) {
 			door[4].getObj().draw(canvas);
 		}
-		if (room.getDownNumber() >= 0) {
-			door[5] = maze.createDoor(room.getDownNumber(), doorObject[5]);
+		if (hasDownDoor()) {
 			door[5].getObj().draw(canvas);
 		}
-		
 		for (int i = 0; i < key.length; i++) {
 			if (roomNumber == key[i].getRoomNumber() && key[i].isItemShow()) {
 				key[i].getObj().draw(canvas);
@@ -140,7 +107,6 @@ public class Main extends GraphicApplication implements MouseObserver {
 				}
 			}
 		}
-		
 		//VERIFICACAO DO CLIQUE NA CHAVE
 		for (int i = 0; i < key.length; i++) {
 			if (key[i].getObj().clicked(p) && key[i].isItemShow()) {
@@ -149,5 +115,84 @@ public class Main extends GraphicApplication implements MouseObserver {
 				redraw();
 			}
 		}
+	}
+	// <-- CONFIGURACAO DO LABIRINTO -->
+	private void initialFiles() {
+		//ARQUIVO QUE CONTEM A CONFIGURACAO DE SALAS DO LABIRINTO
+		pathToFileDungeonMap = "files/Labirinto.txt";
+		//IMAGENS USADAS NOS SPRITES
+		pathToFileHero = "img/hero-noitem.png";
+		pathToFileDoor = "img/dungeon-door.png";
+		pathToFileKey = "img/key.png";
+		pathToFileContainer = "img/square.png";
+	}
+	
+	// <-- LOGICA DO LABIRINTO -->
+	protected void createObjects(){
+		//CRIACAO DO SPRITE DO HEROI
+		containerObject = GameObject.createObject(pathToFileContainer, 0, 525, Color.BLUE);
+		heroObject = GameObject.createObject(pathToFileHero, 375, 250, Color.BLUE);		
+		//CRIACAO DE SPRITES DAS PORTAS E ESCADAS (A.K.A SAIDAS) NORTE-SUL-OESTE-LESTE-ACIMA-ABAIXO (NESTA ORDEM)
+		doorObject[0] = GameObject.createObject(pathToFileDoor, 368, 10, Color.BLUE);
+		doorObject[1] = GameObject.createObject(pathToFileDoor, 368, 522, Color.BLUE);
+		doorObject[2] = GameObject.createObject(pathToFileDoor, 647, 254, Color.BLUE);
+		doorObject[3] = GameObject.createObject(pathToFileDoor, 93, 254, Color.BLUE);
+		doorObject[4] = GameObject.createObject(pathToFileDoor, 470, 150, Color.BLUE);
+		doorObject[5] = GameObject.createObject(pathToFileDoor, 230, 415, Color.BLUE);
+		//CRIACAO DE SPRITES DAS CHAVES
+		keyObject[0] = GameObject.createObject(pathToFileKey, 230, 145, Color.BLUE);
+		keyObject[1] = GameObject.createObject(pathToFileKey, 600, 200, Color.BLUE);
+		keyObject[2] = GameObject.createObject(pathToFileKey, 350, 350, Color.BLUE);
+		keyObject[3] = GameObject.createObject(pathToFileKey, 600, 400, Color.BLUE);
+	}
+	protected Key randomKeys(int i) {
+		Key k = new Key();
+		//int randomRoomNumber = (int)(Math.random()*(31-1));
+		int randomRoomNumber = 10;
+		int randomColor = (int)(Math.random()*(3-0));
+		k = maze.createKeys(randomRoomNumber, randomColor, keyObject[i], true);
+		return k;
+	}
+	private boolean hasNorthDoor(){
+		if (room.getNorthNumber() >= 0) {
+			door[0] = maze.createDoor(room.getNorthNumber(), doorObject[0]);
+			return true;
+		}
+		return false;
+	}
+	private boolean hasSouthDoor(){
+		if (room.getSouthNumber() >= 0) {
+			door[1] = maze.createDoor(room.getSouthNumber(), doorObject[1]);
+			return true;
+		}
+		return false;
+	}
+	private boolean hasEastDoor(){
+		if (room.getEastNumber() >= 0) {
+			door[2] = maze.createDoor(room.getEastNumber(), doorObject[2]);
+			return true;
+		}
+		return false;
+	}
+	private boolean hasWestDoor(){
+		if (room.getWestNumber() >= 0) {
+			door[3] = maze.createDoor(room.getWestNumber(), doorObject[3]);
+			return true;
+		}
+		return false;
+	}
+	private boolean hasUpDoor(){
+		if (room.getUpNumber() >= 0) {
+			door[4] = maze.createDoor(room.getUpNumber(), doorObject[4]);
+			return true;
+		}
+		return false;
+	}
+	private boolean hasDownDoor(){
+		if (room.getDownNumber() >= 0) {
+			door[5] = maze.createDoor(room.getDownNumber(), doorObject[5]);
+			return true;
+		}
+		return false;
 	}
 }
