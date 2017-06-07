10ce0c0171464b23484598cc1b22ec8e3188e12e
Igor Brum <igorsbrum@gmail.com>
2017-06-07 03:00:29 -0300

    Adicionado sprites das chaves e suas posicoes aleatorias nas salas

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 import com.senac.SimpleJava.Console;
 import com.senac.SimpleJava.Graphics.Canvas;
 import com.senac.SimpleJava.Graphics.GraphicApplication;
@@ -10,13 +12,19 @@ import com.senac.SimpleJava.Graphics.events.MouseObserver;
 public class Main extends GraphicApplication implements MouseObserver {
 	public String pathToFileHero, pathToFileDoor, pathToBackground, pathToFileDungeonMap, pathToFileKey;
 	public GameObject doorObject[] = new GameObject[6];
+	public GameObject keyObject[] = new GameObject[4];
 	public Door door[] = new Door[6];
-	public GameObject heroObject, keyObject;
+	public Key key[] = new Key[4];
+	public GameObject heroObject;
 	public Maze maze;
 	public Room room;
 	
 	@Override
 	protected void setup() {
+		Random randomKeyRoomNumber = new Random();
+		Random randomColorKey = new Random();
+		int randomRoomNumber = 0;
+		int randomColor = 0;
 		maze = new Maze();
 		room = new Room();
 		
@@ -28,13 +36,13 @@ public class Main extends GraphicApplication implements MouseObserver {
 		pathToFileDoor = "img/dungeon-door.png";
 		pathToFileKey = "img/key.png";
 		
+		
 		//TITULO DA TELA DO JOGO E RESOLUCAO
 		setTitle("MazeGame");
 		setResolution(Resolution.HIGHRES);
 		
 		//CRIACAO DO SPRITE DO HEROI
 		heroObject = GameObject.createObject(pathToFileHero, 375, 250);
-		keyObject = GameObject.createObject(pathToFileKey, 0, 0);
 		
 		//CRIACAO DE SPRITES DAS PORTAS E ESCADAS (A.K.A SAIDAS) NORTE-SUL-OESTE-LESTE-ACIMA-ABAIXO (NESTA ORDEM)
 		doorObject[0] = GameObject.createObject(pathToFileDoor, 368, 10);
@@ -44,8 +52,23 @@ public class Main extends GraphicApplication implements MouseObserver {
 		doorObject[4] = GameObject.createObject(pathToFileDoor, 470, 150);
 		doorObject[5] = GameObject.createObject(pathToFileDoor, 230, 415);
 		
+		//CRIACAO DE SPRITES DAS CHAVES
+		keyObject[0] = GameObject.createObject(pathToFileKey, 230, 145);
+		keyObject[1] = GameObject.createObject(pathToFileKey, 600, 200);
+		keyObject[2] = GameObject.createObject(pathToFileKey, 350, 350);
+		keyObject[3] = GameObject.createObject(pathToFileKey, 600, 400);
+		
 		//INICIA O LABIRINTO
 		maze.createMaze(pathToFileDungeonMap);
+		
+		//POSICAO ALEATORIA INICIAL DAS CHAVES 
+		for (int i = 0; i < 4; i++) {
+			randomRoomNumber = randomKeyRoomNumber.nextInt(31)+1;
+			randomColor = randomColorKey.nextInt(3)+1;
+			key[i] = maze.createKeys(randomRoomNumber, randomColor, keyObject[i]);
+		}
+		
+		//SALA INICIAL DO LABIRINTO EM MODO ALEATORIO
 		room = maze.randomInitialRoom();
 
 		//MouseObserver PARA CAPTURAR O CLICK NA TELA
@@ -63,7 +86,6 @@ public class Main extends GraphicApplication implements MouseObserver {
 
 		canvas.drawImage(background, 0, 0);
 		heroObject.draw(canvas);
-		keyObject.draw(canvas);
 		canvas.putText(10, 0, 30, "Sala Numero: "+roomNumber);
 		
 		if (room.getNorthNumber() >= 0) {
@@ -90,6 +112,12 @@ public class Main extends GraphicApplication implements MouseObserver {
 			door[5] = maze.createDoor(room.getDownNumber(), doorObject[5]);
 			door[5].getObj().draw(canvas);
 		}
+		
+		for (int i = 0; i < key.length; i++) {
+			if (roomNumber == key[i].getRoomNumber()) {
+				key[i].getObj().draw(canvas);
+			}
+		}
 	}
 	
 	@Override
@@ -97,6 +125,7 @@ public class Main extends GraphicApplication implements MouseObserver {
 	
 	@Override
 	public void notify(MouseEvent evento, int b, Point p) {
+		//VERIFICACAO DO CLIQUE NA PORTA
 		for (int i = 0; i < door.length; i++) {
 			if (door[i] != null) {
 				if (door[i].getObj().clicked(p)) {
