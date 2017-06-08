2e2f1efc65cf42acaabd2d3c2b2187ed0ad19cf5
Igor Brum <igorsbrum@gmail.com>
2017-06-08 16:53:47 -0300

    Adicionado 20170608 005 - Criada classe Armor

---
+++ src/Armor.java
@@ -0,0 +1,19 @@
+
+public class Armor extends Item {
+
+	public String typeArmor;
+	public int armorClass;
+	
+	public String getTypeArmor() {
+		return typeArmor;
+	}
+	public void setTypeArmor(String typeArmor) {
+		this.typeArmor = typeArmor;
+	}
+	public int getArmorClass() {
+		return armorClass;
+	}
+	public void setArmorClass(int armorClass) {
+		this.armorClass = armorClass;
+	}
+}
