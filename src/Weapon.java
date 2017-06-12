ece43bb8fde9dc76964e49bef955179cd1f73338
Igor Brum <igorsbrum@gmail.com>
2017-06-12 11:29:57 -0300

    Adicionado 20170612 002 - Criada classe Weapon

---
+++ src/Weapon.java
@@ -0,0 +1,19 @@
+
+public class Weapon extends Item{
+	private String typeWeapon;
+	private int damageWeapon;
+	private double weaponAccuracy;
+	public String getTypeWeapon() {
+		return typeWeapon;
+	}
+	public void setTypeWeapon(String typeWeapon) {
+		this.typeWeapon = typeWeapon;
+	}
+	public int getDamageWeapon() {
+		return damageWeapon;
+	}
+	public void setDamageWeapon(int damageWeapon) {
+		this.damageWeapon = damageWeapon;
+	}
+
+}
